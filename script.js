597c3119edfca40eae09a5d10af4531c15072535
zerobyte0000 <samsocool0000@gmail.com>
2026-03-15 18:44:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
